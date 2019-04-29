dab2f985576a0cb04de2fc0606d55dd9d062b80b
Noel Yahan <noelyahan@gmail.com>
2019-04-29 13:45:16 +0530

    :tada: Import/Export project init

---
+++ doc.go
@@ -0,0 +1,15 @@
+/*
+Package eximp implements Importer, Exporter interfaces for library inputs and outputs
+
+Loader pkg supports
+
+	FileExporter
+	FileImporter
+	URLImporter
+	AnimationExporter
+
+Loader pkg can be extends to more importers and exporters
+	ex: KafkaImporter, KafkaExporter, MQTT, WebSockets, Serial etc..
+Create your own io and let us know.
+*/
+package eximp // import "github.com/noelyahan/eximp"
